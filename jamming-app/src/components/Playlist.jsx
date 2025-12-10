996a8a91e60f3e8867115ebd36bc44831839f2ee
Dirtmagurt <kking0740@gmail.com>
2025-12-10 09:41:42 -0700

    Added the playlist skeleton and playlist name change, reused Tracklist component to plays data for playlist

---
+++ jamming-app/src/components/Playlist.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TrackList from "./Tracklist";
+
+export default function Playlist ({name, tracks, onNameChange }) {
+    // Handle playlist name change
+    const handleNameChange = (event) => {
+        onNameChange(event.target.value);
+    };
+
+    return (
+        <section className="Playlist">
+            <input type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="New Playlist"
+            />
+
+            <TrackList tracks={tracks} />
+
+        </section>
+    );
+}
