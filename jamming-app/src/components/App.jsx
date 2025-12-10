7d3a38ff360567eb5398fc62d1a1d83c3142c579
Dirtmagurt <kking0740@gmail.com>
2025-12-10 09:23:51 -0700

    Added import for Playlist added playlist component and use states for playlist functionality skeleton

--- jamming-app/src/components/App.jsx
+++ jamming-app/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import SearchResults from "./SearchResults";
+import Playlist from "./Playlist";
+
 
 const mockSearchResutls = [
     {
@@ -24,14 +26,32 @@ const mockSearchResutls = [
     },
 ];
 
+const mockPlaylistTracks = [
+     { id: 101, name: "Save Your Tears", artist: "The Weeknd", album: "After Hours" },
+    { id: 102, name: "Don’t Start Now", artist: "Dua Lipa", album: "Future Nostalgia" },
+
+]
+
 export  default function App() {
     const searchResults = mockSearchResutls; // stand in for real API results
+
+    const [playlistName, setPlaylistName] = useState("My Playlist");
+    const [playlistTracks, setPlaylistTracks] = useState("mockPlaylistTracks");
+
 
     return (
         <div className="App" >
             <h1>Jamming</h1>
 
             <SearchResults tracks={searchResults} />
+
+            <Playlist 
+                name={playlistName}
+                tracks={playlistTracks}
+                onNameChange={setPlaylistName}
+                ///onRemoveTrack, onSave will come later
+                />
+
         </div>
     );
 }
