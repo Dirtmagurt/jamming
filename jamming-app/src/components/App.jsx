e27c65090687e849845207cea7972811f955d07e
Dirtmagurt <kking0740@gmail.com>
2025-12-12 12:27:47 -0700

    added the search bar componenet as well as the on search function added the import from searchbar.jsx and fixed the save playlist syntax error

--- jamming-app/src/components/App.jsx
+++ jamming-app/src/components/App.jsx
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
 import SearchResults from "./SearchResults";
 import Playlist from "./Playlist";
+import Spotify from "../services/spotify";
 
 
 const mockSearchResults = [
-    {
-        id: 1,
-        name: "Blinding Lights",
-        artist: "The Weeknd",
-        album: "After Hours",
-    },
-
-    {
-        id: 2,
-        name: "Levitating",
-        artist: "Dua Lipa",
-        album: "Future Nostalgia",
-    },
-
-    {
-        id: 3,
-        name: "Watermelon Sugar",
-        artist: "Harry Styles",
-        album: "Fine Line",
-    },
+  { id: 1, name: "Blinding Lights", artist: "The Weeknd", album: "After Hours", uri: "spotify:track:0VjIjW4GlUZAMYd2vXMi3b" },
+  { id: 2, name: "Levitating", artist: "Dua Lipa", album: "Future Nostalgia", uri: "spotify:track:463CkQjx2Zk1yXoBuierM9" },
+  { id: 3, name: "Watermelon Sugar", artist: "Harry Styles", album: "Fine Line", uri: "spotify:track:6UelLqGlWMcVH1E5c4H7lY" },
 ];
 
 const mockPlaylistTracks = [
-     { id: 101, name: "Save Your Tears", artist: "The Weeknd", album: "After Hours" },
-    { id: 102, name: "Don’t Start Now", artist: "Dua Lipa", album: "Future Nostalgia" },
+  { id: 101, name: "Save Your Tears", artist: "The Weeknd", album: "After Hours", uri: "spotify:track:5QO79kh1waicV47BqGRL3g" },
+  { id: 102, name: "Don’t Start Now", artist: "Dua Lipa", album: "Future Nostalgia", uri: "spotify:track:3PfIrDoz19wz7qK7tYeu62" },
+];
 
-]
+
 
 export  default function App() {
     const [searchResults] = useState(mockSearchResults); // stand in for real API results
 
     const [playlistName, setPlaylistName] = useState("My Playlist");
-    const [playlistTracks, setPlaylistTracks] = useState("mockPlaylistTracks");
+    const [playlistTracks, setPlaylistTracks] = useState(mockPlaylistTracks);
+
+
+
+    //Added the onSearch function
+    const onSearch = async (term) => {
+    try {
+      const token = await Spotify.getAccessToken();
+      console.log("Spotify token acquired:", token);
+
+      // later:
+      // const results = await Spotify.search(term);
+      // setSearchResults(results);
+
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
 
      //Adding a Track - defining the method
 
@@ -72,7 +76,12 @@ export  default function App() {
             )
         );    
 
-    // Save Playlist (mock)
+ 
+
+
+    };
+
+        // Save Playlist (mock)
     const savePlaylist = () => {
         //creating an array of URIs for Spotify
         const trackUris = playlistTracks.map((track) => track.uri);
@@ -86,10 +95,7 @@ export  default function App() {
         //Resets playlist after export
         setPlaylistName("New Playlist");
         setPlaylistTracks([]);
-    }    
-
-
-    };
+    };   
 
 
     return (
@@ -97,6 +103,9 @@ export  default function App() {
             <h1>Jamming</h1>
 
             {/* Pass addTrack down to SearchResults */}
+
+            <SearchBar onSearch={onSearch} /> 
+
 
             <SearchResults tracks={searchResults} onAddTrack={addTrack} />
 
