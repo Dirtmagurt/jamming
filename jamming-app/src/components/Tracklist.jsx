d7da046d7f74dc332bcf9102fec5133afbdce289
Dirtmagurt <kking0740@gmail.com>
2025-12-14 15:54:27 -0700

    cleaned up logic for tracklist function, passed onRemove not onRemoveTrack

--- jamming-app/src/components/Tracklist.jsx
+++ jamming-app/src/components/Tracklist.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import Track from "./Track";
 
 export default function TrackList({ tracks, onAddTrack, isRemoval, onRemoveTrack, playlistUris }) {
-    return (
-        <div>
+  return (
+    <div>
       {tracks && tracks.length > 0 ? (
         tracks.map((track) => (
           <Track
             key={track.uri}
             track={track}
             onAdd={onAddTrack}
+            onRemove={onRemoveTrack}
             isRemoval={isRemoval}
-            onRemoveTrack={onRemoveTrack}
             isInPlaylist={playlistUris ? playlistUris.has(track.uri) : false}
           />
         ))
       ) : (
         <p>No tracks found.</p>
       )}
-        </div>
-
-    );
+    </div>
+  );
 }
