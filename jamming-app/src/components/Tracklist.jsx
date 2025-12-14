75593e5e185a3bca32dff9f9c11fd1a5ff316e9f
Dirtmagurt <kking0740@gmail.com>
2025-12-14 15:37:10 -0700

    added isInplaylist to track if the song is in playlist or not

--- jamming-app/src/components/Tracklist.jsx
+++ jamming-app/src/components/Tracklist.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Track from "./Track";
 
-export default function TrackList({ tracks, onAddTrack, isRemoval, onRemoveTrack }) {
+export default function TrackList({ tracks, onAddTrack, isRemoval, onRemoveTrack, playlistUris }) {
     return (
         <div>
       {tracks && tracks.length > 0 ? (
         tracks.map((track) => (
           <Track
-            key={track.id}
+            key={track.uri}
             track={track}
             onAdd={onAddTrack}
             isRemoval={isRemoval}
             onRemoveTrack={onRemoveTrack}
+            isInPlaylist={playlistUris ? playlistUris.has(track.uri) : false}
           />
         ))
       ) : (
